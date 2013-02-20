9e5ecf5b68014b34159194606fd8305fb3b60977
Gonsiorowski <RSNTgons@q.ccni.rpi.edu>
2013-02-20 11:17:01 -0500

    better gid==self assertion

--- gates_driver.c
+++ gates_driver.c
@@ -50,7 +50,10 @@ void gates_init(gate_state *s, tw_lp *lp){
     
     int count = sscanf(global_input[gate], "%d %d %d %u %u %u %u", &gid, &output_count, &type, &inputs[0], &inputs[1], &inputs[2], &inputs[3]);
     
-    assert(gid == (self % TOTAL_GATE_COUNT));
+    if (gid != (self % TOTAL_GATE_COUNT)) {
+        printf("gid: %d, self: %d\n", gid, self);
+        assert(gid == (self % TOTAL_GATE_COUNT));
+    }
 
     if (count < 2) {
         printf("Error on %d from reading: \"%s\"\n", self, global_input[gate]);
